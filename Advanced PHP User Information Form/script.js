c79d7b3c52474a7bfa93bcfc19b9e44c304bad14
Deniz Uku <84576822+Dennisz34@users.noreply.github.com>
2021-11-03 09:31:44 +0300

    Advanced PHP User Information Form
    ~~Advanced user information form and video background form I made using PHP and MYSQL
    ~~PHP ve MYSQL kullanarak yaptığım gelişmiş kullanıcı bilgi formu ve videolu arka plan formu

---
+++ Advanced PHP User Information Form/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("change", function(event) {
+    let element = event.target;
+    if (element && element.matches(".form-element-field")) {
+        element.classList[element.value ? "add" : "remove"]("-hasvalue");
+    }
+});
