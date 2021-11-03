b5e7cf799dcb8e5c9a4ba7fade9c4003faca1142
Deniz Uku <84576822+Dennisz34@users.noreply.github.com>
2021-11-03 09:30:12 +0300

    Delete Advanced PHP User Information Form directory

--- Advanced PHP User Information Form/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("change", function(event) {
-    let element = event.target;
-    if (element && element.matches(".form-element-field")) {
-        element.classList[element.value ? "add" : "remove"]("-hasvalue");
-    }
-});
